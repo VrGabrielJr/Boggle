63b34017d8582066b70adfb89e3571d0d9ca612a
Val Ryan Gabriel <gabrielvrcode@gmail.com>
2019-11-03 19:15:30 -0600

    fixed printBoard Method to print correctly

--- Boggle.py
+++ Boggle.py
@@ -1,13 +1,32 @@
+import random
+
 class Boggle:
-  board = [["-" * 6] * 6]
+  board = [["-","-","-","-","-","-"]] * 6
   let = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i','j','k','l','m','n','o','p','q','r',
          's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
   
   def start(self):
-    for i in self.board:
-      for j in i:
-        print(j,)
-      
+    print("Welcome to Text-Base Boggle!")
+    self.printBoard()
+
+  def printBoard(self):
+    for i in range(0,len(self.board)):
+      for j in range(0, len(self.board[i])):
+        print(self.board[i][j], end = " ")
+      print()
+  
+  def shake(self):
+
+    print(len(self.board))
+    for i in range(0,len(self.board)):
+      for j in range(0,len(self.board[0])):
+        num = random.randint(0,25)
+        letter = self.let[num]
+        self.board[i][j] = letter
+        print("i", i, "j", j)
+        print("length of board[0]", len(self.board[i]))
+
+    self.printBoard()
 
 game = Boggle()
 game.start()
