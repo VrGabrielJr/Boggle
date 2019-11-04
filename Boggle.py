0ca8df18a69c8a147ab64d28f3b8c328b58c4662
Val Ryan Gabriel <gabrielvrcode@gmail.com>
2019-11-03 21:47:45 -0600

    created shake method

--- Boggle.py
+++ Boggle.py
@@ -13,15 +13,16 @@ class Boggle:
     for i in range(0, len(self.board), 4):
       print(' '.join(self.board[i:i+4]))
   
-  # def shake(self):
-  #   for i in range(0,len(self.board)):
-  #     for j in range(0,len(self.board[i])):
-  #       num = random.randint(0,25)
-  #       self.board[i][j] = self.let[num]
-  #       print(self.board[i][j], end=" ")
-  #     print()
-  #   print("\n")
-  #   self.printBoard()
+  def shake(self):
+    for i in range(0, len(self.board)):
+      num = random.randint(0,25)
+      self.board[i] = self.let[num]
+    
+    print('\n')
+    print("B O G G L E - B O A R D")
+    print("------------------------")
+    self.printBoard()
 
 game = Boggle()
 game.start()
+game.shake()
