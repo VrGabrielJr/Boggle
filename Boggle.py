55167d6fd6a4e41906c7f93e284e978c0ca4b5d5
Val Ryan Gabriel <gabrielvrcode@gmail.com>
2019-11-03 21:43:37 -0600

    Updated printBoard method

--- Boggle.py
+++ Boggle.py
@@ -1,32 +1,27 @@
 import random
 
 class Boggle:
-  board = [["-","-","-","-","-","-"]] * 6
-  let = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i','j','k','l','m','n','o','p','q','r',
-         's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  board = ["_"] * 16
+  let = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I','J','K','L','M','N','O','P','Q','R',
+         'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   
   def start(self):
     print("Welcome to Text-Base Boggle!")
     self.printBoard()
 
   def printBoard(self):
-    for i in range(0,len(self.board)):
-      for j in range(0, len(self.board[i])):
-        print(self.board[i][j], end = " ")
-      print()
+    for i in range(0, len(self.board), 4):
+      print(' '.join(self.board[i:i+4]))
   
-  def shake(self):
-
-    print(len(self.board))
-    for i in range(0,len(self.board)):
-      for j in range(0,len(self.board[0])):
-        num = random.randint(0,25)
-        letter = self.let[num]
-        self.board[i][j] = letter
-        print("i", i, "j", j)
-        print("length of board[0]", len(self.board[i]))
-
-    self.printBoard()
+  # def shake(self):
+  #   for i in range(0,len(self.board)):
+  #     for j in range(0,len(self.board[i])):
+  #       num = random.randint(0,25)
+  #       self.board[i][j] = self.let[num]
+  #       print(self.board[i][j], end=" ")
+  #     print()
+  #   print("\n")
+  #   self.printBoard()
 
 game = Boggle()
 game.start()
